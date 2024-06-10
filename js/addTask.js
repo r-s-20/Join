d8816c2aa99832c48ccd0f0b51eff1e124af5a9b
@r-s-20 <robin_s_2020@mailbox.org>
2024-06-10 08:41:09 +0200

    addedHelperfunctions

---
+++ js/addTask.js
@@ -0,0 +1,30 @@
+function createNewTask() {
+  let newTask = {
+    title: parseTextInput(getValueFromInput("inputTitle")),
+    timestamp: new Date().getTime(),
+    assigned: getValueFromInput("inputAssigned"),
+    description: parseTextInput(getValueFromInput("inputDescription")),
+    dueDate: getValueFromInput("inputDueDate"),
+    prio: getPrio(),
+    category: getValueFromInput("inputCategory"),
+    subtasks: parseTextInput(getValueFromInput("inputSubtasks")),
+  };
+  console.log(newTask);
+  // newTask.title = ;
+  // newTask.description = ;
+  // newTask.
+}
+
+function addTask() {
+  let newTask = createNewTask();
+  if (validateTask(newTask)) {
+    tasks.push(newTask);
+  }
+}
+ function validateTask(task) {
+    return true;
+ }
+
+ function getPrio() {
+  return "medium";
+ }
--- js/script.js
+++ js/script.js
@@ -1,14 +1,36 @@
 async function includeHTML() {
-    let includeElements = document.querySelectorAll('[w3-include-html]');
-    for (let i = 0; i < includeElements.length; i++) {
-        const element = includeElements[i];
-        file = element.getAttribute("w3-include-html"); // "includes/header.html"
-        let resp = await fetch(file);
-        if (resp.ok) {
-            element.innerHTML = await resp.text();
-        } else {
-            element.innerHTML = 'Page not found';
-        }
+  let includeElements = document.querySelectorAll("[w3-include-html]");
+  for (let i = 0; i < includeElements.length; i++) {
+    const element = includeElements[i];
+    file = element.getAttribute("w3-include-html"); // "includes/header.html"
+    let resp = await fetch(file);
+    if (resp.ok) {
+      element.innerHTML = await resp.text();
+    } else {
+      element.innerHTML = "Page not found";
     }
+  }
+}
+
+let tasks = [
+  {
+    title: "First test title",
+    timestamp: 1717999440032,
+    assigned: "Maria S.",
+    description: "This will be a description",
+    dueDate: "2024-06-12",
+    prio: "medium",
+    category: "Management",
+    subtasks: ["Project structure"],
+  },
+];
+
+function getValueFromInput(inputId) {
+  let container = document.getElementById(inputId);
+  return container.value;
+}
+
+function parseTextInput(string) {
+    return string.trim();
 }
 
