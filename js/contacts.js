e718d28c12013e3c5ef6b2454fff4b5992464c50
Ismail Guendogdu <guendogdu.ismail@gmail.com>
2024-06-19 22:44:11 +0200

    added edit & save contacts

--- js/contacts.js
+++ js/contacts.js
@@ -1,3 +1,5 @@
+let currentContactIndex = null;
+
 function render() {
   document.getElementById("contactListsContainer").innerHTML = "";
 
@@ -8,6 +10,7 @@ function render() {
     if (a.name > b.name) {
       return 1;
     }
+    return 0;
   });
 
   let groupedContacts = {};
@@ -68,27 +71,26 @@ function showContactDetails(index) {
       </div>
 
       <div class="cardHeaderNameDetails">
-          <h1>${contact.name}</h1>
-            <div class="cardHeaderNameEditDelete">
+        <h1>${contact.name}</h1>
+          <div class="cardHeaderNameEditDelete">
              <div class="cardHeaderNameEdit button" onclick="editContactPopUp(${index})">
-                  <img src="img/edit.png" alt="edit Contact Name"/>Edit
-                    </div>
+                <img src="img/edit.png" alt="edit Contact Name"/>Edit
+              </div>
                   <div class="cardHeaderNameDelete button" onclick="deleteContact(${index})">
                        <img src="img/delete.png"alt="delete Contact Name"/>Delete
-                     </div>
-                   </div>
+                    </div>
+                      </div>
                   </div>
                   </div>
                   <div class="contactCardInfo">
                   <h4>Contact Information</h4>
                 </div>
                 <div class="contactCardDetails">
-                
                     <div class="cardDetailEmailName">
-                    <p><h2>Email</h2> <a href="mailto:${contact.email}">${contact.email}</a></p>
+                    <p><h2>Email</h2> <a>${contact.email}</a></p>
                     </div>
-                    <div class="CardDetailPhoneNumber"><h2>Phone</h2> ${contact.phone}</div>
-                  </div>
+            <div class="CardDetailPhoneNumber"><h2>Phone</h2> ${contact.phone}</div>
+      </div></div>
   `;
 
   let contactDetailsContainer = document.getElementById("contactCardMain");
@@ -97,18 +99,42 @@ function showContactDetails(index) {
 }
 
 function addContactPopUp() {
+  document.getElementById("overlayBackground").style.display = "block";
   document.getElementById("showAddContactPopUp").classList.remove("d-none");
 }
 
 function closeAddContactPopUp() {
+  document.getElementById("overlayBackground").style.display = "none";
   document.getElementById("showAddContactPopUp").classList.add("d-none");
 }
 
 function editContactPopUp(index) {
-  document.getElementById("showEditContactPopUp").classList.remove("d-none");
+  let contact = contacts[index];
+  currentContactIndex = index;
+  let editName = document.getElementById("editName");
+  let editEmail = document.getElementById("editEmail");
+  let editPhone = document.getElementById("editPhone");
+  let editInitials = document.getElementById("editInitials");
+  let saveEditButton = document.getElementById("saveEditButton");
+  let editBadge = document.querySelector(".overlayEditContactRightSideBadge h1");
+
+  if (editName && editEmail && editPhone && editInitials && editBadge) {
+    editName.value = contact.name;
+    editEmail.value = contact.email;
+    editPhone.value = contact.phone;
+    editInitials.value = contact.initials;
+    editBadge.innerText = contact.initials;
+    editBadge.style.backgroundColor = contact.badgeColor;
+    saveEditButton.onclick = function() {
+      saveContact(index);
+    };
+    document.getElementById("overlayBackground").style.display = "block";
+    document.getElementById("showEditContactPopUp").classList.remove("d-none");
+  } 
 }
 
 function closeEditContactPopUp() {
+  document.getElementById("overlayBackground").style.display = "none";
   document.getElementById("showEditContactPopUp").classList.add("d-none");
 }
 
@@ -116,6 +142,19 @@ function addContact() {
   let name = document.getElementById("addName").value;
   let email = document.getElementById("addEmail").value;
   let phone = document.getElementById("addPhone").value;
+
+  let contactExists = false;
+
+  contacts.forEach(contact => {
+    if (contact.name === name || contact.email === email) {
+      contactExists = true;
+    }
+  });
+
+  if (contactExists) {
+    alert("Ein Kontakt mit diesem Namen oder dieser Email-Adresse existiert bereits.");
+    return;
+  }
 
   let names = name.split(" ");
   let initials = "";
@@ -139,8 +178,46 @@ function addContact() {
   closeAddContactPopUp();
 }
 
+function saveContact(index) {
+  let name = document.getElementById("editName").value;
+  let email = document.getElementById("editEmail").value;
+  let phone = document.getElementById("editPhone").value;
+
+  let contactExists = false;
+  contacts.forEach((contact, j) => {
+    if (j !== index && (contact.name === name || contact.email === email)) {
+      contactExists = true;
+    }
+  });
+
+  if (contactExists) {
+    alert("Ein Kontakt mit diesem Namen oder dieser Email-Adresse existiert bereits.");
+    return;
+  }
+
+  let names = name.split(" ");
+  let initials = "";
+  if (names.length > 0) {
+    initials += names[0][0].toUpperCase();
+    if (names.length > 1) {
+      initials += names[names.length - 1][0].toUpperCase();
+    }
+  }
+
+  contacts[index].name = name;
+  contacts[index].email = email;
+  contacts[index].phone = phone;
+  contacts[index].initials = initials;
+
+  render();
+  showContactDetails(index);
+
+  closeEditContactPopUp();
+}
+
 function deleteContact(index) {
   contacts.splice(index, 1);
   render();
   document.getElementById("contactCardMain").classList.add("d-none");
+  closeEditContactPopUp();
 }
