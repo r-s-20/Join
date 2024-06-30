7a743be9aded4bac8a43e76cd6817be62dd5cbea
@r-s-20 <robin_s_2020@mailbox.org>
2024-06-30 09:25:15 +0200

    added disable past dates function for calendar

--- js/addTask.js
+++ js/addTask.js
@@ -10,8 +10,24 @@ async function init() {
   }
   await includeHTML();
   renderUserlogo();
+  disablePastCalendarDates();
   await loadTasksFromAPI();
   await loadContactsFromAPI();
+}
+
+/**Disables all past dates (before today) in calendar input html element */
+function disablePastCalendarDates() {
+  let inputDate = document.getElementById("inputDueDate");
+  var dtToday = new Date();
+
+  var month = dtToday.getMonth() + 1;
+  var day = dtToday.getDate();
+  var year = dtToday.getFullYear();
+  if (month < 10) month = "0" + month.toString();
+  if (day < 10) day = "0" + day.toString();
+  var maxDate = year + "-" + month + "-" + day;
+  //   $("#inputdate").attr("min", maxDate);
+  inputDate.setAttribute("min", maxDate);
 }
 
 /** Adds a new task from task form to the tasks-array
